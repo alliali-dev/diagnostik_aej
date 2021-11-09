69f02742daf8571f80d5c4f9d8370270f39d8843
utilisateur <17311182+rg4amia@users.noreply.github.com>
2021-11-09 11:19:24 +0000

    export excell

--- app/Exports/RencontreChefAgence.php
+++ app/Exports/RencontreChefAgence.php
@@ -3,6 +3,8 @@
 namespace App\Exports;
 
 use App\Models\Rencontre;
+use App\Models\User;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -13,11 +15,46 @@ class RencontreChefAgence implements FromCollection,WithHeadings
     */
     public function collection()
     {
-        return Rencontre::mine()->get();
+        $entretiendiag  = Rencontre::mine()->get();
+        $data = [];
+        foreach($entretiendiag as $item){
+            $data[]=[
+                'id'                => $item->suivirencontre_id,
+                'matricule_aej'     => $item->suivirencontre->matricule_aej,
+                'nomprenom'         => $item->suivirencontre->nomprenom,
+                'sexe'              => $item->suivirencontre->sexe,
+                'lieudereisdence'   => $item->suivirencontre->lieudereisdence,
+                'diplome'           => $item->suivirencontre->diplome,
+                'dureerencontre'    => $item->dureerencontre,
+                'dateprochainrdv'   => Carbon::parse($item->dateprochainrdv)->format('M d, Y'),
+                'axetravail'        => $item->axetravail,
+                'conseiller'        => User::where('agence_id', session()->get('orig_agence'))->where('id',$item->user_id)->first()->name
+            ];
+        }
+        //return Rencontre::mine()->get();
+        return collect($data);
     }
 
     public function headings(): array
     {
-        return array_keys($this->collection()->first()->toArray());
+        return [
+            'matriculeaej',
+            'nomprenom',
+            'niveauformaion',
+            'niveauexperience',
+            'adeqormaexper',
+            'conmetieractiv',
+            'adqformmetieractiv',
+            'adqexpmetieractiv',
+            'maitoutrechempl',
+            'conexigmarch',
+            'depdossent',
+            'conseiller'
+        ];
     }
+
+    /*public function headings(): array
+    {
+        return array_keys($this->collection()->first()->toArray());
+    }*/
 }
