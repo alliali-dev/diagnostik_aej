354fd8db962763a9f2a7e11e2465dc30b071cf9f
r4gamia <17311182+rg4amia@users.noreply.github.com>
2022-06-29 09:24:55 +0000

    Correction du 29 juin 2022

--- resources/views/layouts/inc/message.blade.php
+++ resources/views/layouts/inc/message.blade.php
@@ -2,6 +2,5 @@
     <div class="alert alert-danger">
         <button type="button" class="close" data-dismiss="alert">×</button>
         Une erreur est survenue dans le formulaire :
-        {{ implode('', $errors->all('<div>:message</div>')) }}
-    </div>
+        {{ implode('', $errors->all(':message</div>')) }}
 @endif
