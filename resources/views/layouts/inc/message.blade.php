75284d6781d6aaad74e4721dc3745b5e92a6341c
r4gamia <17311182+rg4amia@users.noreply.github.com>
2022-06-29 09:28:29 +0000

    Correction du 29 juin 2022

--- resources/views/layouts/inc/message.blade.php
+++ resources/views/layouts/inc/message.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
     <div class="alert alert-danger">
         <button type="button" class="close" data-dismiss="alert">×</button>
-        Une erreur est survenue dans le formulaire :
-        {{ implode('', $errors->all(':message')) }}
+        Veuillez joindre la grille entretien et la grille de diagnostique.
+      {{--  {{ implode('', $errors->all(':message <br>')) }}--}}
     </div>
 @endif
